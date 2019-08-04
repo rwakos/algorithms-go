1b9753b1df61e15bdf6aed71d5ebafd013071458
Richard Reveron <rwakos@gmail.com>
2019-08-04 10:52:11 +0200

    Remove duplicates in a Linked List Node

---
+++ Cracking-The-Code-Interview/linked-lists/main.go
@@ -0,0 +1,61 @@
+package main
+
+import "fmt"
+
+/*
+LinkedListNode simple linked list
+*/
+type LinkedListNode struct {
+	//previous *LinkedListNode
+	data int
+	next *LinkedListNode
+}
+
+func main() {
+	t := []int{1, 2, 3, 4, 5, 2, 1, 3, 4, 5}
+	l := new(LinkedListNode)
+	l = nil
+	for _, v := range t {
+		item := &LinkedListNode{v, nil}
+		l = addNode(item, l)
+	}
+	l = removeDuplicates(l)
+	printLinkedList(l)
+}
+
+func removeDuplicates(lln *LinkedListNode) *LinkedListNode {
+	cache := map[int]bool{}
+	l := lln
+	cache[l.data] = true
+	for l.next != nil {
+		_, exists := cache[l.next.data]
+		cache[l.next.data] = true
+		if exists {
+			l.next = l.next.next
+		} else {
+			l = l.next
+		}
+	}
+
+	return lln
+}
+
+func printLinkedList(lln *LinkedListNode) {
+	for l := lln; l != nil; l = l.next {
+		fmt.Print("->", l.data)
+	}
+	fmt.Println("")
+}
+
+func addNode(newItem, lln *LinkedListNode) *LinkedListNode {
+	if lln == nil {
+		return newItem
+	}
+	for l := lln; l != nil; l = l.next {
+		if l.next == nil {
+			l.next = newItem
+			return lln
+		}
+	}
+	return lln
+}
